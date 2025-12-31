ef31a24473722338d29645e8165feca17c62acc3
rohanulislam <rohanulislam.ict@gmail.com>
2025-12-31 12:09:34 +0600

    Create WelcomeApp.java

---
+++ connectdb/src/WelcomeApp.java
@@ -0,0 +1,11 @@
+public class WelcomeApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println("i = " + counter);
+        }
+    }
+}
